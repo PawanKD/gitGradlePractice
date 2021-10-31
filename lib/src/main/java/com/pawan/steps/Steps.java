32acf38464883d6641c9b44a4ea80c6b5a311881
PawanKD <PawanKD@DESKTOP-8TSJ7B9>
2021-10-31 22:13:22 +0530

    [Gradle First Commit]

---
+++ lib/src/main/java/com/pawan/steps/Steps.java
@@ -0,0 +1,13 @@
+package com.pawan.steps;
+
+public class Steps {
+	
+	public void stepMethod() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Steps Working Fine");
+		System.out.println("Steps2 Working Fine");
+		
+	}
+
+}
